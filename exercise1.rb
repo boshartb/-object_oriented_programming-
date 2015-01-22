05f996b0d2422d1cd11f58d3ddbfa0342618342f
Brant <boshartb@gmail.com>
2015-01-22 17:12:04 -0500

    Printing correct, pre error

--- exercise1.rb
+++ exercise1.rb
@@ -1,17 +1,38 @@
+class Person #this is a class with named person
+  attr_accessor :name, :email, :age # 
+
+	def initialize(name)
+		@name = name
+	end
+
+	def greeting
+    " Hi, my name is #{name}."
+  end
+end
+
 class Student  
 	def learn
-		puts "I get it!"
+		"I get it!"
 	end
 end
 
 class Instructor 
 	def teach
-		puts "Everything in Ruby is an Object!"
+		"Everything in Ruby is an Object!"
 	end
 end
+	
 
+instructor = Person.new("Chris")	
+puts instructor.greeting
+i = Instructor.new
+puts i.teach
+student = Person.new("Christina")
+puts student.greeting
 s = Student.new
 puts s.learn
 
-i = Instructor.new
-puts i.teach
+
+
+
+
